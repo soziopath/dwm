9151f5bfee476265bc0bf90c3c22aa4ea875e058
soziopath <a.j.schaeffler@googlemail.com>
2026-03-16 17:40:14 +0100

    feh floating rule

--- config.def.h
+++ config.def.h
@@ -68,6 +68,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            0,           -1 },
+	{ "feh",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
